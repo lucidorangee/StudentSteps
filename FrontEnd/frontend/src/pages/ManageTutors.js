e0946020cc24108683c345e94cb6bc8b943abdc1
Jihoo Ahn <lucidorangee@gmail.com>
2024-09-04 13:41:53 -0400

    removed unused authentication check from managetutors

--- FrontEnd/frontend/src/pages/ManageTutors.js
+++ FrontEnd/frontend/src/pages/ManageTutors.js
@@ -5,18 +5,6 @@ import { Nav, Navbar } from 'react-bootstrap'
 const ManageUsers = () => {
   const [tutors, setTutors] = useState(null);
   const navigate = useNavigate();
-/*
-  useEffect(() => {
-    //Fetch authentication status
-    fetch('http://localhost:4000/users/status')
-      .then(response => response.json())
-      .then(data => {
-        setAuthenticated(data.isAuthenticated);
-      })
-      .catch(error => {
-        console.error('Error fetching authentication status: ', error);
-      })
-  }, []);*/
 
   useEffect(() => {
     //Fetch authentication status
@@ -35,18 +23,6 @@ const ManageUsers = () => {
         console.error('Error fetching tutors data: ', error);
       })
   }, []);
-  
-  //{JSON.stringify(comments, null, 2)}
-    return (
-      <div>
-        {authenticated ? (
-          <AuthenticatedOptions tutors={tutors} />
-        ) : (
-          <UnauthenticatedOptions />
-        )}
-      </div>
-    );
-  };
 
   const handleDelete = (tutor_id) => {
     fetch(`${process.env.REACT_APP_API_BASE_URL}/tutors/${tutor_id}`, {
@@ -78,58 +54,48 @@ const ManageUsers = () => {
     navigate(`/tutors/detail/${tutor_id}`, { replace : true});
 
   }
-
-  const AuthenticatedOptions = ( {tutors} ) => {    
-    return (
-      <div className="App">
-        <h2>Welcome, User!</h2>
-        <table className="table">
-          <thead>
-            <tr>
-              <th>Tutor ID</th>
-              <th>User ID</th>
-              <th>Name</th>
-              <th>Phone Number</th>
-              <th>Email</th>
-              <th>Delete</th>
+  
+  return (
+    <div className="App">
+      <h2>Welcome, User!</h2>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Tutor ID</th>
+            <th>User ID</th>
+            <th>Name</th>
+            <th>Phone Number</th>
+            <th>Email</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+        {Array.isArray(tutors) && (tutors).length > 0 ? (
+          (tutors).map((tutor, index) => (
+            <tr key={index}>
+              <td onClick={() => redirectTutorProfile(tutor.tutor_id)}>{tutor.tutor_id}</td>
+              <td>{tutor.user_id}</td>
+              <td>{tutor.first_name} {tutor.last_name}</td>
+              <td>{tutor.contact_phone}</td>
+              <td>{tutor.contact_email}</td>
+              <td>
+                <i
+                  className="bi bi-trash"
+                  style={{ cursor: 'pointer' }}
+                  onClick={() => handleDelete(tutor.tutor_id)}
+                ></i>
+              </td>
             </tr>
-          </thead>
-          <tbody>
-          {Array.isArray(tutors) && (tutors).length > 0 ? (
-            (tutors).map((tutor, index) => (
-              <tr key={index}>
-                <td onClick={() => redirectTutorProfile(tutor.tutor_id)}>{tutor.tutor_id}</td>
-                <td>{tutor.user_id}</td>
-                <td>{tutor.first_name} {tutor.last_name}</td>
-                <td>{tutor.contact_phone}</td>
-                <td>{tutor.contact_email}</td>
-                <td>
-                  <i
-                    className="bi bi-trash"
-                    style={{ cursor: 'pointer' }}
-                    onClick={() => handleDelete(tutor.tutor_id)}
-                  ></i>
-                </td>
-              </tr>
-            ))):(
-              <tr>
-                <td colSpan="9">No data available</td>
-              </tr>
-            )}
-          </tbody>
-        </table>
-      </div>
-      
-    );
-  };
-
-  const UnauthenticatedOptions = () => {
+          ))):(
+            <tr>
+              <td colSpan="9">No data available</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </div>
     
-    return (
-      <div>
-        <h2>Please Log In</h2>
-      </div>
-    );
-  };
+  );
 
+};
   export default ManageUsers;
