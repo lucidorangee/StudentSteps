3329ea4560d635f0f1e866af09f61ad2894dacdd
Jihoo Ahn <lucidorangee@gmail.com>
2024-09-15 20:27:40 -0400

    datetime output

--- FrontEnd/frontend/src/pages/ManageDueHomework.js
+++ FrontEnd/frontend/src/pages/ManageDueHomework.js
@@ -26,6 +26,7 @@ const ManageDueHomework = () => {
     e.preventDefault();
     
     // Convert the date string (yyyy/MM/dd) into a Date object
+    console.log(datetime);
     const [year, month, day] = datetime.split('/').map(Number); // split and convert to numbers
     const targetDate = new Date(year, month - 1, day); // month is zero-indexed in JS Date (Jan = 0)
     const targetDateOnly = new Date(targetDate.getFullYear(), targetDate.getMonth(), targetDate.getDate());
