9353c6d3195ff280d4f2385c5b1353a5bae3c826
Jihoo Ahn <lucidorangee@gmail.com>
2024-09-15 21:55:36 -0400

    Changed due date comparison from equal to less than or equal to

--- FrontEnd/frontend/src/pages/ManageDueHomework.js
+++ FrontEnd/frontend/src/pages/ManageDueHomework.js
@@ -36,7 +36,7 @@ const ManageDueHomework = () => {
       const givenDate = new Date(homeworkList[i].due_date);
       const givenDateOnly = new Date(givenDate.getFullYear(), givenDate.getMonth(), givenDate.getDate());
       console.log(givenDate.getTime());
-      if(targetDateOnly.getTime() === givenDateOnly.getTime())
+      if(targetDateOnly.getTime() <= givenDateOnly.getTime())
       {
         temp.push(homeworkList[i]);
       }
