d5ce09bf3b92227ea487cacecfc27a4ec2a0ca44
Jihoo Ahn <lucidorangee@gmail.com>
2024-09-22 22:56:43 -0400

    added export default

--- FrontEnd/frontend/src/ProtectedRouteWrapper.js
+++ FrontEnd/frontend/src/ProtectedRouteWrapper.js
@@ -4,3 +4,5 @@ import ProtectedRoute from './ProtectedRoute.js';
 const ProtectedRouteWrapper = ({ element: Component, ...rest }) => {
     return <ProtectedRoute element={Component} {...rest} />;
 };
+
+export default ProtectedRouteWrapper;
