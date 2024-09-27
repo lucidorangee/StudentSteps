cef8f33ad0b70947b2c8b0e83a4027e8e96b257a
Jihoo Ahn <lucidorangee@gmail.com>
2024-09-27 12:19:00 -0400

    fixed managehomework

--- FrontEnd/frontend/src/pages/ManageHomework.js
+++ FrontEnd/frontend/src/pages/ManageHomework.js
@@ -2,11 +2,59 @@ import React, { useState, useEffect } from 'react';
 import { Outlet, Link, NavLink, useNavigate } from "react-router-dom";
 import { Nav, Navbar } from 'react-bootstrap'
 import Select from 'react-select';
+import { useQuery,  useQueryClient, useMutation } from '@tanstack/react-query';
+import { Navigate } from 'react-router-dom';
+
+const fetchHomework = async () => {
+  const response = await fetch(`${process.env.REACT_APP_API_BASE_URL}/homework`, {
+    credentials: 'include',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error('Failed to fetch homework');
+  }
+  return response.json();
+};
+
+const fetchStudents = async () => {
+  const response = await fetch(`${process.env.REACT_APP_API_BASE_URL}/students`, {
+    credentials: 'include',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error('Failed to fetch students');
+  }
+  return response.json();
+};
+
+const deleteHomeworkByID = async (id) => {
+  const response = await fetch(`${process.env.REACT_APP_API_BASE_URL}/homework/${id}`, {
+    credentials: 'include',
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  if (!response.ok) {
+    const responseText = await response.text();
+    throw new Error('Failed to delete homework: ' + responseText); // Include responseText in the error for context
+  }
+
+  return;
+}
 
 const ManageUsers = () => {
-  const [students, setStudents] = useState(null);
-  const [homework, setHomework] = useState(null);
+  //const [students, setStudents] = useState(null);
+  //const [homework, setHomework] = useState(null);
   const navigate = useNavigate();
+  const queryClient = useQueryClient();
 
   
   const [loading, setLoading] = useState(true);
@@ -16,7 +64,56 @@ const ManageUsers = () => {
   
   const [filteredHomework, setFilteredHomework] = useState(null);
 
+  const {
+    data: students,
+    isLoading: studentsLoading,
+    error: studentsError,
+  } = useQuery({queryKey: ['students'], queryFn: () => fetchStudents()});
 
+  const {
+    data: homeworkList,
+    isLoading: homeworkListLoading,
+    error: homeworkListError,
+  } = useQuery({queryKey: ['homework'], queryFn: () => fetchHomework()});
+
+  const { mutate: deleteHomework, isLoading, isError, error } = useMutation({
+    mutationFn: (id) => deleteHomeworkByID(id),
+    onSuccess: () => {
+      queryClient.invalidateQueries(['homework']);
+      console.log("Successfully deleted");
+    },
+    onError: (error) => {
+      console.log('Error deleting homework:', error.message);
+    }
+  });
+
+  useEffect(() => {
+    setFilteredHomework(data);
+  }, [homeworkList])
+
+  // End of Hooks
+
+  if (homeworkListLoading || studentsLoading) return <div>Loading...</div>;
+  if (homeworkListError) {
+    if(homeworkListError.status === 401) //unauthorized
+    {
+      console.log("unathorized");
+      return <Navigate to="/login" />;
+    }
+    console.log(homeworkListError.status);
+    return <div>Error loading data</div>;
+  }
+  if (studentsError) {
+    if(studentsError.status === 401) //unauthorized
+    {
+      console.log("unathorized");
+      return <Navigate to="/login" />;
+    }
+    console.log(studentsError.status);
+    return <div>Error loading data</div>;
+  }
+
+/*
   useEffect(() => {
     fetch(`${process.env.REACT_APP_API_BASE_URL}/students/`, {
       credentials: 'include',
@@ -51,8 +148,8 @@ const ManageUsers = () => {
       .catch(error => {
         console.error('Error fetching homework data: ', error);
       })
-  }, []);
-
+  }, []);*/
+/*
   const handleDelete = (homework_id) => {
     fetch(`${process.env.REACT_APP_API_BASE_URL}/homework/${homework_id}`, {
       credentials: 'include',
@@ -76,10 +173,10 @@ const ManageUsers = () => {
     .catch(error => {
       console.error('Error deleting session:', error);
     });
-  };
+  };*/
 
   const redirectStudentProfile  = (homework_id) => {
-    navigate(`/homwork/detail/${homework_id}`, { replace : true});
+    navigate(`/homework/detail/${homework_id}`, { replace : true});
   }
 
   const handleFilterNameChange = (e) => {
@@ -96,7 +193,7 @@ const ManageUsers = () => {
     e.preventDefault();
   
     let temp = [];
-    for(let i = 0; i < homework.length; i++)
+    for(let i = 0; i < homeworkList.length; i++)
     {
       if((students[i].first_name.includes(filterName) || students[i].last_name.includes(filterName))
         && (filterGrade === 0 || students[i].grade_level === filterGrade))
@@ -161,7 +258,7 @@ const ManageUsers = () => {
                 <i
                   className="bi bi-trash"
                   style={{ cursor: 'pointer' }}
-                  onClick={() => handleDelete(ahomework.homework_id)}
+                  onClick={() => deleteHomework(ahomework.homework_id)}
                 ></i>
               </td>
             </tr>
