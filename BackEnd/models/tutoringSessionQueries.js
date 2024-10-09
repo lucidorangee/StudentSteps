68212281971134a978f761f0d7c6ba36f546b697
Jihoo Ahn <lucidorangee@gmail.com>
2024-10-09 14:18:54 -0400

    set default of is_completed of homework creation to be 0

--- BackEnd/models/tutoringSessionQueries.js
+++ BackEnd/models/tutoringSessionQueries.js
@@ -1,6 +1,6 @@
 const getTutoringSessions = "SELECT * FROM tutoring_session";
 const getTutoringSessionById = "SELECT * FROM tutoring_session WHERE session_id = $1";
-const addTutoringSession = "INSERT INTO tutoring_session (student_id, tutor_id, session_datetime, duration, notes) VALUES ($1, $2, $3, $4, $5)";
+const addTutoringSession = "INSERT INTO tutoring_session (student_id, tutor_id, session_datetime, duration, notes, is_completed) VALUES ($1, $2, $3, $4, $5, 0)";
 const removeTutoringSession = "DELETE FROM tutoring_session WHERE session_id = $1";
 const completeTutoringSession = "UPDATE tutoring_session SET complete = true WHERE session_id = $1";
 
