968e1492a4ce270f0ceaf9d18a40c604f06ac1ee
Jihoo Ahn <lucidorangee@gmail.com>
2024-10-22 13:21:42 -0400

    revert to button

--- FrontEnd/frontend/src/pages/ScheduleDaily.js
+++ FrontEnd/frontend/src/pages/ScheduleDaily.js
@@ -348,10 +348,18 @@ const ScheduleDaily = () => {
 
                       return (
                         
-                        <td key={`${tutor_id}-${colIndex}-${timeIndex}} className="session-cell" rowSpan={col[timeIndex].length + 1`}>
-                          <div className="session" style={{ backgroundColor: `#${intToHexSpread(tutor_id)}` }}>{col[timeIndex].student}<br />
-                          {new Intl.DateTimeFormat('en-US', timeonlySetting).format(col[timeIndex].start)} - {new Intl.DateTimeFormat('en-US', timeonlySetting).format(col[timeIndex].end)}</div>
+                        <td key={`${tutor_id}-${colIndex}-${timeIndex}`} className="session-cell" rowSpan={col[timeIndex].length + 1}>
+                          <Button
+                            style={{ width: '100%', height: '100%' }} // Ensures the button fills the entire cell
+                            onClick={handleShow}
+                          >
+                            <div className="session" style={{ backgroundColor: `#${intToHexSpread(tutor_id)}` }}>
+                              {col[timeIndex].student}<br />
+                              {new Intl.DateTimeFormat('en-US', timeonlySetting).format(col[timeIndex].start)} - {new Intl.DateTimeFormat('en-US', timeonlySetting).format(col[timeIndex].end)}
+                            </div>
+                          </Button>
                         </td>
+
                       );
                     });
                   })
