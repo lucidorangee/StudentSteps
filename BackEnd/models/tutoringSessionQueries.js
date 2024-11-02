2e171288cd942b2a4eee819be072eed62e7a4602
Jihoo Ahn <lucidorangee@gmail.com>
2024-11-02 16:54:33 -0400

    updated var name

--- BackEnd/models/tutoringSessionQueries.js
+++ BackEnd/models/tutoringSessionQueries.js
@@ -4,7 +4,7 @@ const addTutoringSession = "INSERT INTO tutoring_session (student_id, tutor_id, 
 const removeTutoringSessionById = "DELETE FROM tutoring_session WHERE session_id = $1";
 const removeTutoringSessionByStudentId = "DELETE FROM tutoring_session WHERE student_id = $1 AND complete = false";
 const removeTutoringSessionByTutorId = "DELETE FROM tutoring_session WHERE tutor_id = $1 AND complete = false";
-const removeTutoringSessionsBetweenDates = "DELETE FROM tutoring_sessions WHERE student_id = $1 AND session_datetime BETWEEN $2 AND $3";
+const removeTutoringSessionsBetweenDates = "DELETE FROM tutoring_session WHERE student_id = $1 AND session_datetime BETWEEN $2 AND $3";
 const completeTutoringSession = "UPDATE tutoring_session SET complete = true WHERE session_id = $1";
 const rollbackTutoringSession = "UPDATE tutoring_session SET complete = false WHERE session_id = $1";
 
