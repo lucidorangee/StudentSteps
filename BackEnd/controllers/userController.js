a0d6e58cd3c2a8d3e9cd0ebfa847fa29491f896a
Jihoo Ahn <lucidorangee@gmail.com>
2024-11-21 13:46:23 -0500

    fixed logout

--- BackEnd/controllers/userController.js
+++ BackEnd/controllers/userController.js
@@ -25,9 +25,13 @@ const getUsers = (req, res) => {
   
 }
 
-const logout = (req, res) => {
-  req.logout();
-  res.status(200).json({ message: "Logged out successfully" });
+const logout = (req, res, next) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    res.status(200).json({ message: "Logged out successfully" });
+  });
 };
 
 /*
