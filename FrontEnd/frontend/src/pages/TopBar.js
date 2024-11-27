0d2b3bbdd107a9d84eaacab502ffe59614affb60
Jihoo Ahn <lucidorangee@gmail.com>
2024-11-26 22:30:42 -0500

    updated topbar to look more organized

--- FrontEnd/frontend/src/pages/TopBar.js
+++ FrontEnd/frontend/src/pages/TopBar.js
@@ -1,32 +1,63 @@
 import React from 'react';
-import { Outlet, Link, NavLink } from "react-router-dom";
-import { Nav, Navbar } from 'react-bootstrap'
-import { LinkContainer } from 'react-router-bootstrap';
+import { Nav, Navbar } from 'react-bootstrap';
+import { NavLink } from "react-router-dom";
+import { useUser } from '../components/UserContext';
 
 const Topbar = () => {
-  return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div className="container-fluid">
-            <a href="#" className="navbar-brand">StudentSteps</a>
-            <button type="button" className="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarCollapse">
-                <div className="navbar-nav">
-                    <Nav.Link as={NavLink} to='/admin/analysis' exact>Admin</Nav.Link>
-                    <Nav.Link as={NavLink} to='/schedule/list' exact>Schedule</Nav.Link>
-                    <Nav.Link as={NavLink} to='/edcoordinator' exact>EdCoordinator</Nav.Link>
-                    <Nav.Link as={NavLink} to='/profile' exact>Profile</Nav.Link>
-                </div>
-                <div className="navbar-nav ms-auto">
-                    <Nav.Link as={NavLink} to='/register' exact>Register</Nav.Link>
-                    <Nav.Link as={NavLink} to='/login' exact>Login</Nav.Link>
-                    <Nav.Link as={NavLink} to='/logout' exact>Logout</Nav.Link>
+    const { user } = useUser();
+
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className="container-fluid">
+                <a href="#" className="navbar-brand">StudentSteps</a>
+                <button 
+                    type="button" 
+                    className="navbar-toggler" 
+                    data-bs-toggle="collapse" 
+                    data-bs-target="#navbarCollapse"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarCollapse">
+                    <div className="navbar-nav">
+                        <Nav.Link as={NavLink} to='/admin/analysis' exact>Admin</Nav.Link>
+                        <Nav.Link as={NavLink} to='/schedule/list' exact>Schedule</Nav.Link>
+                        <Nav.Link as={NavLink} to='/edcoordinator' exact>EdCoordinator</Nav.Link>
+                        <Nav.Link as={NavLink} to='/profile' exact>Profile</Nav.Link>
+                    </div>
+                    <div className="navbar-nav ms-auto">
+                        {user ? (
+                            <div className="nav-item dropdown">
+                                <a 
+                                    href="#" 
+                                    className="nav-link dropdown-toggle" 
+                                    id="userDropdown" 
+                                    role="button" 
+                                    data-bs-toggle="dropdown" 
+                                    aria-expanded="false"
+                                >
+                                    {user.name}
+                                </a>
+                                <ul className="dropdown-menu dropdown-menu-end" aria-labelledby="userDropdown">
+                                    <li>
+                                        <Nav.Link as={NavLink} to='/profile' className="dropdown-item">Profile</Nav.Link>
+                                    </li>
+                                    <li>
+                                        <Nav.Link as={NavLink} to='/logout' className="dropdown-item">Logout</Nav.Link>
+                                    </li>
+                                </ul>
+                            </div>
+                        ) : (
+                            <>
+                                <Nav.Link as={NavLink} to='/register' exact>Register</Nav.Link>
+                                <Nav.Link as={NavLink} to='/login' exact>Login</Nav.Link>
+                            </>
+                        )}
+                    </div>
                 </div>
             </div>
-        </div>
-    </nav>
-  );
+        </nav>
+    );
 };
 
 export default Topbar;
